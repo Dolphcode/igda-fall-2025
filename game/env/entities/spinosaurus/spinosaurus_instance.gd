3e74d6e081a648220155763e74e98078dd5b3c50
Sebastian Vargas <49503850+Dolphcode@users.noreply.github.com>
2025-09-27 12:36:31 -0400

    Added ability to make spinosaurus disappear

--- game/env/entities/spinosaurus/spinosaurus_instance.gd
+++ game/env/entities/spinosaurus/spinosaurus_instance.gd
@@ -3,6 +3,10 @@ extends Sprite2D
 class_name SpinosaurusInstance
 
 
+var screens_on: int = 0
+
+var lifetime: float = 0.5
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,3 +17,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	modulate.a += delta / 255.0 * 10
+	if screens_on == 0:
+		lifetime -= delta
+		
+	if lifetime <= 0:
+		queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	screens_on += 1
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	screens_on -= 1
