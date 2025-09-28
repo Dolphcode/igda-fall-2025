5a935635d524f9f91d05087f6f13a709b9dc6562
Sebastian Vargas <49503850+Dolphcode@users.noreply.github.com>
2025-09-27 22:16:34 -0400

    Randomize car directions

--- game/minigame/map/map_gen.gd
+++ game/minigame/map/map_gen.gd
@@ -87,11 +87,16 @@ func gen_row(cell_row: int, chunk_type: int) -> void:
 				obstacle_layer.set_cell(Vector2i(i, cell_row), 0, Vector2i(1, chunk_type))
 				
 	elif chunk_type == ChunkType.ROAD:
-		var pos: Vector2 = obstacle_layer.map_to_local(Vector2i(max_col, cell_row))
+		var l_or_r = randi_range(0, 1)
+		var spawn_x = max_col if l_or_r == 0 else min_col
+		var dir = -1 if l_or_r == 0 else 1
+		
+		var pos: Vector2 = obstacle_layer.map_to_local(Vector2i(spawn_x, cell_row))
 		pos = to_local(obstacle_layer.to_global(pos))
 		var obj: Spawner = car_spawner.instantiate()
 		call_deferred("add_child", obj)
 		obj.position = pos
+		obj.direction = dir
 		obj.spawner_lower_bound = spawner_lower_bound
 		
 		obj.speed = [0.01, 0.02, 0.04, 0.05].pick_random()
