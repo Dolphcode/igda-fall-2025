76275cb0832e5dd62603bc06a595209c04096e13
Sebastian Vargas <49503850+Dolphcode@users.noreply.github.com>
2025-09-28 16:21:59 -0400

    Removed some print statements

--- game/minigame/lose_area.gd
+++ game/minigame/lose_area.gd
@@ -2,6 +2,5 @@ extends Area2D
 ## Simple object shredder
 
 func _on_area_entered(area: Area2D):
-	print(area.get_parent().get_groups())
 	if area.get_parent().is_in_group("player"):
 		get_tree().root.get_node("Game").lose_game("Boundaries")
